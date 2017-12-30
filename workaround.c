ad2af9c897cfe16da320ec1f0b0105a191f3e0c3
Holmes Futrell <holmes@gingerlabs.com>
2017-12-30 15:44:37 -0800

    added workaround for missing symbol referenced in fmod library.

--- workaround.c
+++ workaround.c
@@ -5,4 +5,7 @@
 //  Created by Holmes Futrell on 12/30/17.
 //
 
-#include "workaround.h"
+// this is a workaround hack to libfmodx86 referencing a function OTCountDataBytes. I don't know where this function is supposed to come from.
+void OTCountDataBytes(void) {
+    ;
+}
