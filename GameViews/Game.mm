310360f2a40567a5574b34dbf82247d81253c00b
Holmes Futrell <holmes@gingerlabs.com>
2017-12-30 15:42:52 -0800

    #warning macro about hack.

--- GameViews/Game.mm
+++ GameViews/Game.mm
@@ -327,10 +327,11 @@ static Game *sharedInstance;
     //if ([[NSApplication sharedApplication] isActive] && playerShip){ //only if app is in front!
         
         timeSinceLastControlSend+=FRAME;
-        
+    
         KeyMap keys;
         GetKeys( keys );
-          
+
+#warning I had to hack this with the typecast to get key input working
         unsigned char *theKeyMap = (unsigned char *)(&keys);
         if ( IsKeyDown( theKeyMap, MAC_ARROW_RIGHT_KEY ) || IsKeyDown( theKeyMap, MAC_G_KEY )) {
 		
